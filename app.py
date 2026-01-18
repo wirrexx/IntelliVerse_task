d480d19daac97c66a91b55e02df1866b62aaaf68
Wisam Odish <wirrexx@gmail.com>
2026-01-18 20:06:13 +0100

    Function mistral: added url, headers, payload, created a response and return data tes -not tested

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-#import requests
+import requests
 import os
 
 app = Flask(__name__)
@@ -7,10 +7,27 @@ app = Flask(__name__)
 MISTRAL_API_KEY = os.getenv("MISTRAL_API_KEY")
 OPENROUTE_API_KEY = os.getenv("OPENROUTE_API_KEY")
 
-def call_mistral_api(prompt: str) -> str:
-    # query Mistral API
-    pass
 
+
+def call_mistral_api(prompt: str) -> str:
+    url = "https://api.mistral.ai/v1/chat/completions "
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {MISTRAL_API_KEY}"
+        }
+    
+    payload = {
+        "model":"mistral-large-latest",
+        "message": [{"role": "user", "content": prompt}]
+        }
+    
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
+    data = response.json()
+    return data["choices"][0]["message"]["content"]
+    
+
+   
 def call_openroute_api(prompt: str) -> str:
     # query OpenRoute API
     pass
